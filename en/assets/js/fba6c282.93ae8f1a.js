5689c960e739ccc8f6214248fe45360854fb3503
danyow <i.zuucol@gmail.com>
2021-12-22 15:23:24 +0000

    Deploy website - based on f84628da679c601f3b90e23bc7ed63571aec65a9

---
+++ en/assets/js/fba6c282.93ae8f1a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdanyow=self.webpackChunkdanyow||[]).push([[4364],{9501:function(e){e.exports=JSON.parse('{"permalink":"/en/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
