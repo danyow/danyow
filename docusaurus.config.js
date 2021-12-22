e38f708b9e737f4822da2be521dd14476bf8a7fa
danyow <i.zuucol@gmail.com>
2021-12-22 12:10:00 +0800

    github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -136,7 +136,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/tutorial/intro',
+                to: '/docs/greeting',
               },
             ],
           },
